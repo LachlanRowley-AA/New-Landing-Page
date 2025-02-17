dafde8007fcca10b28c56b71e4a2a0c5d5278be2
LachlanRowley-AA <lachlan@assetalley.com.au>
2025-02-17 15:32:45 +1100

    added api

--- components/AuthenticationForm.tsx
+++ components/AuthenticationForm.tsx
@@ -1,15 +1,13 @@
 'use client';
 
 import { useState } from 'react';
-import { IconAt, IconLock } from '@tabler/icons-react';
+import { IconAt, IconHash } from '@tabler/icons-react';
 import {
-  Anchor,
   Button,
   Checkbox,
   Group,
   LoadingOverlay,
   Paper,
-  PasswordInput,
   Text,
   TextInput,
   Container,
@@ -17,6 +15,12 @@ import {
 } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { JumboTitle } from './JumboTitle';
+import { createClient } from '@supabase/supabase-js';
+import validator from 'validator';
+
+const supabase = createClient("https://hfsysehrdshrbtmjsgcx.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhmc3lzZWhyZHNocmJ0bWpzZ2N4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0MjMyMzIsImV4cCI6MjA1NDk5OTIzMn0.S2q4Oza4s70afQlODHW-G3OUWIGWxOJ2nOxIzZJ8IIk")
+const windowUrl = window.location.search;
+const params = new URLSearchParams(windowUrl);
 
 export interface AuthenticationFormProps {
   noShadow?: boolean;
@@ -31,44 +35,80 @@ export function AuthenticationForm({
   noSubmit,
   style,
 }: AuthenticationFormProps) {
-  const [formType, setFormType] = useState<'register' | 'login'>('register');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  const toggleFormType = () => {
-    setFormType((current) => (current === 'register' ? 'login' : 'register'));
-    setError(null);
-  };
 
   const form = useForm({
     initialValues: {
       firstName: '',
       lastName: '',
       email: '',
-      password: '',
-      confirmPassword: '',
+      phone: '',
       contact: false,
-      marketing: false
+      ref: ''
     },
   });
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     setLoading(true);
     setError(null);
-    setTimeout(() => {
+  
+    let hasError = false;
+    if(form.values.contact === false) {
+      form.setFieldError('contact', 'Please agree');
+      hasError = true;
+    }
+
+    if (!form.values.firstName.trim()) {
+      form.setFieldError('firstName', 'First name is required');
+      hasError = true;
+    }
+  
+    if (!form.values.lastName.trim()) {
+      form.setFieldError('lastName', 'Last name is required');
+      hasError = true;
+    }
+  
+    if (!validator.isEmail(form.values.email)) {
+      form.setFieldError('email', 'Invalid email address');
+      hasError = true;
+    }
+  
+    if (!validator.isMobilePhone(form.values.phone, 'en-AU')) {
+      form.setFieldError('phone', 'Invalid Australian phone number');
+      hasError = true;
+    }
+
+
+  
+    if (hasError) {
       setLoading(false);
-      setError(
-        formType === 'register'
-          ? 'User with this email already exists'
-          : 'User with this email does not exist'
-      );
-    }, 3000);
+      return;
+    }
+  
+    await submitFormAPI();
+    setLoading(false);
   };
+  
+  async function submitFormAPI () {
+    const { error } = await supabase
+    .from('enquiries')
+    .insert({first_name: form.values.firstName,
+            last_name: form.values.lastName,
+            phone: form.values.phone,
+            email: form.values.email,
+            referral: params.get('ref')
+    });
+  }
 
   return (
     <Container size="xs" px="md" style={{ height:'vh:100px'}} mt="calc(var(--mantine-spacing-xl) * 1)">
             <JumboTitle order={2} fz="sm" ta="center" style={{ textWrap: 'balance' }} mb="sm">
             Contact Us Now
+            </JumboTitle>
+            <JumboTitle order={2} fz="xxs" ta="center" style={{ textWrap: 'balance' }} mb="sm">
+            For a free enquiry with no impact to your credit score
             </JumboTitle>
         <Flex justify="center" align="center">
             <Paper
@@ -82,7 +122,6 @@ export function AuthenticationForm({
                 >
             <form onSubmit={form.onSubmit(handleSubmit)}>
                 <LoadingOverlay visible={loading} />
-                {formType === 'register' && (
                 <Group grow>
                     <TextInput
                     data-autofocus
@@ -99,8 +138,14 @@ export function AuthenticationForm({
                     {...form.getInputProps('lastName')}
                     />
                 </Group>
-                )}
-
+                <TextInput
+                mt="md"
+                required
+                placeholder="Your phone Number"
+                label="Phone"
+                leftSection={<IconHash size={16} stroke={1.5} />}
+                {...form.getInputProps('phone')}
+                />
                 <TextInput
                 mt="md"
                 required
@@ -111,13 +156,8 @@ export function AuthenticationForm({
                 />
                 <Checkbox
                     mt="xl"
-                    label="I agree that I may be contacted by a member of Asset Alley"
+                    label="I agree to be contacted by a member of Asset Alley"
                     {...form.getInputProps('contact', { type: 'checkbox' })}
-                />
-                <Checkbox
-                    mt="xl"
-                    label="I agree to receive marketing materials"
-                    {...form.getInputProps('marketing', { type: 'checkbox' })}
                 />
 
                 {error && (
@@ -129,7 +169,7 @@ export function AuthenticationForm({
                 {!noSubmit && (
                 <Group justify="center" mt="xl">
                     <Button color="#01E194" type="submit">
-                    {formType === 'register' ? 'Submit' : 'Submit'}
+                      Submit
                     </Button>
                 </Group>
                 )}
