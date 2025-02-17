ebb2a55d4b4f8ae2b494562cc24aef11e86b5887
LachlanRowley-AA <lachlan@assetalley.com.au>
2025-02-17 15:50:02 +1100

    temp removed ref

--- components/AuthenticationForm.tsx
+++ components/AuthenticationForm.tsx
@@ -19,7 +19,7 @@ import { createClient } from '@supabase/supabase-js';
 import validator from 'validator';
 
 const supabase = createClient("https://hfsysehrdshrbtmjsgcx.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhmc3lzZWhyZHNocmJ0bWpzZ2N4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0MjMyMzIsImV4cCI6MjA1NDk5OTIzMn0.S2q4Oza4s70afQlODHW-G3OUWIGWxOJ2nOxIzZJ8IIk")
-const windowUrl = window.location.search;
+// const windowUrl = window.location.search;
 
 export interface AuthenticationFormProps {
   noShadow?: boolean;
@@ -91,26 +91,25 @@ export function AuthenticationForm({
   };
   
   async function submitFormAPI () {
-    if(typeof window !== "undefined") {
-      const params = new URLSearchParams(windowUrl);
+    // if(typeof window !== "undefined") {
+    //   const params = new URLSearchParams(windowUrl);
+    //   const { error } = await supabase
+    //   .from('enquiries')
+    //   .insert({first_name: form.values.firstName,
+    //           last_name: form.values.lastName,
+    //           phone: form.values.phone,
+    //           email: form.values.email,
+    //           referral: params.get('ref')
+    //   });
+    // }
+    //   else {
       const { error } = await supabase
       .from('enquiries')
       .insert({first_name: form.values.firstName,
               last_name: form.values.lastName,
               phone: form.values.phone,
-              email: form.values.email,
-              referral: params.get('ref')
-      });
-    }
-      else {
-        const { error } = await supabase
-        .from('enquiries')
-        .insert({first_name: form.values.firstName,
-                last_name: form.values.lastName,
-                phone: form.values.phone,
-                email: form.values.email
-      });
-    }    
+              email: form.values.email
+    });
   }
 
   return (
