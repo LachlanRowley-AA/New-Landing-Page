e30824ff9bdd6c27d82e8301994461984d423199
LachlanRowley-AA <lachlan@assetalley.com.au>
2025-05-16 15:54:25 +1000

    Commented out logo

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -11,13 +11,13 @@ export function Welcome() {
   return (
     <>
       <motion.div initial={{ opacity: 0.0, y: 0 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
-      <Flex justify="center" align="center" direction="row" wrap="nowrap" pt="xs" px={0} mx={0}>
+      {/* <Flex justify="center" align="center" direction="row" wrap="nowrap" pt="xs" px={0} mx={0}>
         <NextImage
           src={image}
           alt="Logo"
           style={{ height: '100%', width: 'auto', maxWidth: '80vw' }}
         />
-      </Flex>
+      </Flex> */}
       <JumboTitle c="#01E194" ta="center" fz="lg" maw={580} mx="auto" mt="xl" fw={700}>
         Finance your websites and software
       </JumboTitle>
